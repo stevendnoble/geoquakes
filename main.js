7fccbaadffbbe3e0e079da1ce714253989c04a93
stevennoble78 <stevennoble78@hotmail.com>
2015-11-06 11:35:21 -0800

    needs work

--- main.js
+++ main.js
@@ -5,4 +5,24 @@ $(function() {
   var $quakesList = $('#quakes-list');
   var map;
 
+  $.get('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson', function(data) {
+			console.log(data.features);
+			var quakeResults = data.features;
+			console.log(quakeResults);
+
+			var time = new Date().getTime();
+			var date = new Date(time);
+			console.log(date.toString());
+			console.log(time.toString());
+
+		  // Compile handlebars template
+		  var source = $('#quakes-template').html();
+		  var template = Handlebars.compile(source);
+
+		  // Pass in HTML and append to page
+		  var quakeHTML = template({quakes: quakeResults});
+		  $quakesList.append(quakeHTML);
+		});
+
+
 });
